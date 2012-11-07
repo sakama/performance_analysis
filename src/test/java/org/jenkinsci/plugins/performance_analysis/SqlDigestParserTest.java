56c9950a39e075eaf39070203dbfb7b8504df28f
sakama <satoshi777v@gmail.com>
2012-11-08 00:15:43 +0900

    pt-query-digestの実行結果ファイルのパーサとテストケースを追加

---
+++ src/test/java/org/jenkinsci/plugins/performance_analysis/SqlDigestParserTest.java
@@ -0,0 +1,14 @@
+package org.jenkinsci.plugins.performance_analysis;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SqlDigestParserTest {
+
+    @Test
+    public void test() {
+        fail("Not yet implemented");
+    }
+
+}
