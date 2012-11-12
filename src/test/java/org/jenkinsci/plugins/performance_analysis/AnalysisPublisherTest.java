56312bac54b634f7559ca4e0551adf009f2b4c63
sakama <satoshi777v@gmail.com>
2012-11-12 20:44:08 +0900

    JUnitのテストケース追加したが、まだ動かない。

--- src/test/java/org/jenkinsci/plugins/performance_analysis/AnalysisPublisherTest.java
+++ src/test/java/org/jenkinsci/plugins/performance_analysis/AnalysisPublisherTest.java
@@ -1,14 +1,18 @@
 package org.jenkinsci.plugins.performance_analysis;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
+import static org.hamcrest.CoreMatchers.*;
+import hudson.util.FormValidation;
+
+import java.io.IOException;
+import javax.servlet.ServletException;
 
 public class AnalysisPublisherTest {
 
     @Test
-    public void test() {
-        //fail("Not yet implemented");
+    public void doCheckPtpathTest() throws IOException, ServletException {
+        //FormValidation actual = AnalysisPublisher.DESCRIPTOR.doCheckPtpath("");
+        //assertEquals(actual, FormValidation.error("パスが入力されていません。"));
     }
-
 }
