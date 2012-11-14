1c8b9e84ec8ff13054f0be45c7ecd49e99351af0
sakama <satoshi777v@gmail.com>
2012-11-14 13:04:53 +0900

    AnalysisReportPublisherクラスのJUnitテストを書いた

--- src/test/java/org/jenkinsci/plugins/performance_analysis/AnalysisReportPublisherTest.java
+++ src/test/java/org/jenkinsci/plugins/performance_analysis/AnalysisReportPublisherTest.java
@@ -2,7 +2,6 @@ package org.jenkinsci.plugins.performance_analysis;
 
 import static org.junit.Assert.*;
 import org.junit.Test;
-import static org.hamcrest.CoreMatchers.*;
 import hudson.util.FormValidation;
 
 import java.io.IOException;
@@ -11,8 +10,17 @@ import javax.servlet.ServletException;
 public class AnalysisReportPublisherTest {
 
     @Test
-    public void doCheckPtpathTest() throws IOException, ServletException {
-        //FormValidation actual = AnalysisPublisher.DESCRIPTOR.doCheckPtpath("");
-        //assertEquals(actual, FormValidation.error("パスが入力されていません。"));
+    public void フォーム要素onblur時のバリデーションチェック() throws IOException, ServletException {
+        FormValidation actual = AnalysisReportPublisher.DESCRIPTOR.doCheckPtpath("/var/");
+        assertEquals(actual, FormValidation.ok());
+        
+        FormValidation actual2 = AnalysisReportPublisher.DESCRIPTOR.doCheckPtpath("");
+        assertEquals(actual2.toString(), FormValidation.error("パスが入力されていません。").toString());
+        
+        FormValidation actual3 = AnalysisReportPublisher.DESCRIPTOR.doCheckPtpath("/var/notexist");
+        assertEquals(actual3.toString(), FormValidation.error("ディレクトリが存在しません。").toString());
+        
+        FormValidation actual4 = AnalysisReportPublisher.DESCRIPTOR.doCheckPtpath("/etc/sudoers");
+        assertEquals(actual4.toString(), FormValidation.error("ディレクトリに読み取り権限がありません。").toString());
     }
 }
